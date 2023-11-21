4b19b58a72828c8fed5a58f89514e65c14ecdd56
TylerIllman <tyler.illman.productions@gmail.com>
2023-11-21 11:49:01 +0800

    light and dark mode toggle

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,3 +1,4 @@
+import { ModeToggle } from "@/components/mode-toggle";
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 import { UserButton } from "@clerk/nextjs";
@@ -7,6 +8,7 @@ export default function Home() {
     return (
         <div>
             <UserButton afterSignOutUrl="/" />
+            <ModeToggle />
         </div>
     );
 }
