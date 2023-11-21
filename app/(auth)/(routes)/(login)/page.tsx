70cf21f89a3a981c837bdd84469e98da6f3d85d6
TylerIllman <tyler.illman.productions@gmail.com>
2023-11-21 11:45:10 +0800

    add theme provider

--- app/(auth)/(routes)/(login)/page.tsx
+++
@@ -1,7 +0,0 @@
-import { Button } from "@/components/ui/button";
-import { cn } from "@/lib/utils";
-const state = true;
-
-export default function Home() {
-    return <div>this is NOT protected route</div>;
-}
--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,7 +1,12 @@
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
+import { UserButton } from "@clerk/nextjs";
 const state = true;
 
 export default function Home() {
-    return <div>this is a protected route</div>;
+    return (
+        <div>
+            <UserButton afterSignOutUrl="/" />
+        </div>
+    );
 }
