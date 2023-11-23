c01f29ee00df08a694ddc4164662bd7fe419e399
TylerIllman <tyler.illman.productions@gmail.com>
2023-11-23 21:47:50 +0800

    add nav bar seperator

--- components/navigation/navigation-sidebar.tsx
+++ components/navigation/navigation-sidebar.tsx
@@ -2,6 +2,7 @@ import { NavigationAction } from "./navigation-action";
 import { db } from "@/lib/db";
 import { currentProfile } from "@/lib/current-profile";
 import { redirect } from "next/navigation";
+import { Separator } from "@/components/ui/separator";
 
 const NavigationSideBar = async () => {
     const profile = await currentProfile();
@@ -23,6 +24,7 @@ const NavigationSideBar = async () => {
     return (
         <div className="space-y-4 flex flex-col items-center h-full text-primary w-full dark:bg-[#1E1F22] py-3">
             <NavigationAction />
+            <Separator className="h-[2px] bg-zinc-300 dark:bg-zinc-700 rounded-md w-10 mx-auto" />
         </div>
     );
 };
