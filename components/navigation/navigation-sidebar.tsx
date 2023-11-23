8ba4b6415e4faca43848a42ddfa23b20c0f8ab2d
TylerIllman <tyler.illman.productions@gmail.com>
2023-11-23 20:56:43 +0800

    begin work on servers and nav bar page

---
+++ components/navigation/navigation-sidebar.tsx
@@ -0,0 +1,29 @@
+import { db } from "@/lib/db";
+import { currentProfile } from "@/lib/current-profile";
+import { redirect } from "next/navigation";
+
+const NavigationSideBar = async () => {
+    const profile = await currentProfile();
+
+    if (!profile) {
+        return redirect("/");
+    }
+
+    const severs = await db.server.findMany({
+        where: {
+            members: {
+                some: {
+                    profileId: profile.id,
+                },
+            },
+        },
+    });
+
+    return (
+        <div className="space-y-4 flex flex-col items-center h-full text-primary w-full dark:bg-[#1E1F22] py-3">
+            Nav
+        </div>
+    );
+};
+
+export default NavigationSideBar;
