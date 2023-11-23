3dca920ff8df0e4ca3d0022b133da5210acf19d0
TylerIllman <tyler.illman.productions@gmail.com>
2023-11-24 06:27:30 +0800

    base component for server side bar

---
+++ app/(main)/(routes)/servers/[serverId]/layout.tsx
@@ -0,0 +1,41 @@
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { redirectToSignIn } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+const ServerIdLayout = async ({
+    children,
+    params,
+}: {
+    children: React.ReactNode;
+    params: { serverId: string };
+}) => {
+    const profile = await currentProfile();
+
+    if (!profile) {
+        return redirectToSignIn();
+    }
+    const server = await db.server.findUnique({
+        where: {
+            id: params.serverId,
+            members: {
+                some: {
+                    profileId: profile.id,
+                },
+            },
+        },
+    });
+
+    if (!server) {
+        return redirect("/");
+    }
+
+    return (
+        <div className="h-full">
+            <div className="hidden md:flex h-full w-60 z-20 flex-col fixed inset-y-0"></div>
+            <main className="md:pl-60 h-full">{children}</main>
+        </div>
+    );
+};
+
+export default ServerIdLayout;
