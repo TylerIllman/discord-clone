ab473999c4860b866a52b48992fd2623b93102d1
TylerIllman <tyler.illman.productions@gmail.com>
2023-11-24 12:52:02 +0800

    Edit server

---
+++ app/api/servers/[serverId]/route.ts
@@ -0,0 +1,39 @@
+import { db } from "@/lib/db";
+import { v4 as uuidv4 } from "uuid";
+import { currentProfile } from "@/lib/current-profile";
+import { NextResponse } from "next/server";
+
+export async function PATCH(
+    req: Request,
+    {params}: {params: {serverId: string}},
+) {
+    try {
+        const profile = await currentProfile();
+        const { name, imageUrl } = await req.json();
+
+        if (!profile) {
+            return new NextResponse("Unauthorized", { status: 401 });
+        }
+
+        if (!params.serverId) {
+            return new NextResponse("Server ID Missing", { status: 400 });
+        }
+
+        const server = await db.server.update({
+            where: {
+                id: params.serverId,
+                profileId: profile.id,
+            },
+            data: {
+                name,
+                imageUrl,
+            }
+        });
+
+        return NextResponse.json(server);
+        
+    } catch (error) {
+        console.log("[SERVER_ID_PATCH]", error);
+        return new NextResponse("Internal Error", { status: 500 });
+    }
+}
