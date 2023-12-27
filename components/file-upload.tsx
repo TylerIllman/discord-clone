73769dc43c791e2ffad6066ee405e5c557b31f15
TylerIllman <tyler.illman.productions@gmail.com>
2023-12-27 11:31:30 +0800

    send attachemnts modal

--- components/file-upload.tsx
+++ components/file-upload.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { UploadDropzone } from "@/lib/uploadthing";
-import { X } from "lucide-react";
+import { FileIcon, X } from "lucide-react";
 import Image from "next/image";
 
 import "@uploadthing/react/styles.css";
@@ -22,6 +22,29 @@ export const FileUpload = ({ onChange, value, endpoint }: FileUploadProps) => {
                 <button
                     onClick={() => onChange("")}
                     className="bg-rose-500 text-white p-1 rounded-full absolute top-0 right-0 shadow"
+                    type="button"
+                >
+                    <X className="h-4 w-4" />
+                </button>
+            </div>
+        );
+    }
+
+    if (value && fileType === "pdf") {
+        return (
+            <div className="relative flex items-center p-2 mt-2 rounded-md bg-background/10">
+                <FileIcon className="h-10 w-10 fill-indigo-200 stroke-indigo-400" />
+                <a
+                    href={value}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="ml-2 text-sm text-indigo-500 dark:text-indigo-400 hover:underline"
+                >
+                    {value}
+                </a>
+                <button
+                    onClick={() => onChange("")}
+                    className="bg-rose-500 text-white p-1 rounded-full absolute -top-2 -right-2 shadow"
                     type="button"
                 >
                     <X className="h-4 w-4" />
