28454dfdbd14fc000fc96640732bfb4cf240b0ae
TylerIllman <tyler.illman.productions@gmail.com>
2023-12-27 16:41:10 +0800

    add message component basics

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import { ThemeProvider } from "@/components/providers/theme-provider";
 import { cn } from "@/lib/utils";
 import { ModalProvider } from "@/components/providers/modal-provider";
 import { SocketProvider } from "@/components/providers/socket-provier";
+import { QueryProvider } from "@/components/providers/query-provider";
 
 const font = Open_Sans({ subsets: ["latin"] });
 
@@ -33,7 +34,7 @@ export default function RootLayout({
                     >
                         <SocketProvider>
                             <ModalProvider />
-                            {children}
+                            <QueryProvider>{children}</QueryProvider>
                         </SocketProvider>
                     </ThemeProvider>
                 </body>
---
+++ hooks/use-chat-query.ts
@@ -0,0 +1,56 @@
+import qs from "query-string"
+import { useInfiniteQuery } from "@tanstack/react-query"
+
+import { useSocket } from "@/components/providers/socket-provier"
+
+interface ChatQueryProps {
+    queryKey: string;
+    apiUrl: string;
+    paramKey: "channelId" | "conversationId";
+    paramValue: string;
+}
+
+export const useChatQuery = ({
+    queryKey,
+    apiUrl,
+    paramKey,
+    paramValue,
+}: ChatQueryProps) => { 
+    const {isConnected } = useSocket()
+
+    const fetchMessages = async ({pageParm = undefined}) => {
+        const url = qs.stringifyUrl({
+            url: apiUrl,
+            query: {
+                cursor: pageParm,
+                [paramKey]: paramValue,
+            }
+        }, {skipNull: true})
+
+        const res = await fetch(url)
+        return res.json()
+    }
+
+    const {
+        data,
+        fetchNextPage, 
+        hasNextPage,
+        isFetchingNextPage,
+        status,
+    } = useInfiniteQuery({
+        queryKey: [queryKey],
+        queryFn: fetchMessages,
+        getNextPageParam: (lastPage) => lastPage?.nextCursor,
+        refetchInterval: isConnected ? false : 1000,
+    })
+
+    return {
+        data,
+        fetchNextPage,
+        hasNextPage,
+        isFetchingNextPage,
+        status,
+    }
+}
+
+  
