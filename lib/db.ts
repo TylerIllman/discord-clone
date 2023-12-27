214d71a504a5e9b634c3e271fb99f0b8b57b6460
TylerIllman <tyler.illman.productions@gmail.com>
2023-12-28 07:49:51 +0800

    attempt to fix deployment error

--- lib/db.ts
+++
@@ -1,9 +0,0 @@
-import { PrismaClient } from "@prisma/client";
-
-declare global {
-    var prisma: PrismaClient;
-    }
-
-export const db = global.prisma || new PrismaClient();
-
-if (process.env.NODE_ENV === "development") global.prisma = db;
